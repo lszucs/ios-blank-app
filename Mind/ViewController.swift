36c37b4bd61d19df6bead94ad0a889379261e52f
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-07-23 22:48:48 +0200

    initial commit

---
+++ Mind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mind
+//
+//  Created by Laszlo Szucs on 2019. 07. 23..
+//  Copyright © 2019. LaszloSzucsOrg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
